88806bba0fde3b6bbb83f85f9b9bdd13faaf9bc2
thuynt99 <43128159+thuynt99@users.noreply.github.com>
2018-10-13 10:06:52 +0700

    Add files via upload

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -24,4 +24,48 @@ public class DictionaryCommandline {
         advan.dictionaryManagement.dictionaryLookup();
 
     }
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        DictionaryCommandline test = new DictionaryCommandline();
+        test.dictionaryManagement.insertFromFile();
+        int option;
+        do {
+            System.out.println("---------------------------------");
+            System.out.println("*****Wellcome My Dictionary******");
+            System.out.println("----------Your Selection---------");
+            System.out.println("1. Tra tu");
+            System.out.println("2. Tim kiem chinh xac");
+            System.out.println("3. Them tu");
+            System.out.println("4. Sua hoac xoa tu");
+            System.out.println("5. In tat ca cac tu trong tu dien");
+            System.out.println("6. Luu ket qua vao file moi");
+            System.out.println("7. Ket thuc");
+            System.out.println("---------------------------------");
+            option = scan.nextInt();
+            switch (option) {
+                case 1:
+                    test.dictionaryManagement.dictionarySearcher();
+                    break;
+                case 2:
+                    test.dictionaryManagement.dictionaryLookup();
+                    break;
+                case 3:
+                    test.dictionaryManagement.insertFromCommandline();
+                    break;
+                case 4:
+                    test.dictionaryManagement.dictionaryFix();
+                    break;
+                case 5:
+                    test.showAllwords();
+                    break;
+                case 6:
+                    test.dictionaryManagement.dictionaryExportToFile();
+                    break;
+                case 7:
+                    System.out.println("Thank for watching");
+                    break;
+            }
+        }
+        while (option >= 1 && option <= 6);
+    }
 }
